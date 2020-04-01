0e8a07c5ab9ca28fc920c891a6dc95ffeb218560
pavneetkaur27 <pavneetkaur.jag@gmail.com>
2020-04-01 22:46:22 +0530

    added constant file

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const NODE_VALUES = {
+    "CONTANIER_NODE"    : '1',
+    "LEAF_NODE"         : '2'
+}
+
+export const NODE_VALUES_CONSTANT = {
+    1 : "Contanier Node",
+    2 : "Leaf Node"
+}
+
