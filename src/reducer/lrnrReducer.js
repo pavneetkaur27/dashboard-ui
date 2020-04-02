15391d11d99a3a2977488d783acfe77fac1d1821
pavneetkaur27 <pavneetkaur.jag@gmail.com>
2020-04-03 01:20:33 +0530

    updated reducer

--- src/reducer/lrnrReducer.js
+++ src/reducer/lrnrReducer.js
@@ -1,10 +1,35 @@
 const initialState = {
-    
+    loading : false
 }
 
 export default function (state = initialState, action) {
     switch (action.type) {
+
+        case 'START_LOADER' : {
+            return {...state,
+                loading : true
+            }
+        }
         
+        case 'STOP_LOADER' : {
+            return {...state,
+                loading : false
+            }
+        }
+
+        case 'ADD_CONTANIER_NODES' : {
+            return {
+                ...state,
+                contaniernodes : action.payload.contaniernodes
+            }
+        }
+
+        case "FETCH_EDITOR_CONTENT":{
+            return {
+                ...state,
+                editorcontent :action.payload.editorcontent
+            }
+        }
     
         default:
             return state;
