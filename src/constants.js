7b0dae56463bb71d1e29a1013a4ac942b15d3483
pavneetkaur27 <pavneetkaur.jag@gmail.com>
2020-04-03 01:19:59 +0530

    updated constants

--- src/constants.js
+++ src/constants.js
@@ -8,3 +8,6 @@ export const NODE_VALUES_CONSTANT = {
     2 : "Leaf Node"
 }
 
+
+export const Lorem = {"blocks":[{"key":"99c5e","text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut velit metus, interdum quis risus id, varius lacinia neque. Suspendisse eget luctus lectus. Phasellus nulla neque, dignissim semper nisi id, interdum porta lorem. Donec in justo tellus. Curabitur placerat mi neque, sed luctus lectus interdum sodales. ","type":"unstyled","depth":0,"inlineStyleRanges":[{"offset":0,"length":26,"style":"BOLD"}],"entityRanges":[],"data":{}},{"key":"29ovb","text":"Cras vel viverra ante. Mauris ac condimentum dui, et venenatis quam. Integer pellentesque convallis placerat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a elit aliquam, vulputate leo quis, sodales massa. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras pharetra mollis consequat. Vivamus rutrum libero non imperdiet consectetur. Integer auctor metus at libero fringilla","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}
+
